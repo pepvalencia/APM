8cfa9d4e994a5fa66368b8e28dc0fddd1fb12369
pepvalencia <pepvalencia@users.noreply.github.com>
2021-11-29 14:34:07 +0100

    Agregar archivos de proyecto.

---
+++ src/app/products/product-detail.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  pageTitle: string = 'Product Detail';
+  errorMessage = '';
+  product: IProduct | undefined;
+  sub!: Subscription;
+
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService) {
+  }
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pageTitle += `:${id}`;
+    this.sub = this.productService.getProduct(id).subscribe({
+      next: product => {
+        this.product = product;
+      },
+      error: err => this.errorMessage = err
+    });
+   
+  }
+
+  getProduct(id: number): void {
+    this.productService.getProduct(id).subscribe({
+      next: product => this.product = product,
+      error: err => this.errorMessage = err
+    });
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
+
+}
---
+++ src/app/products/product.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, throwError } from "rxjs";
+import { catchError, tap ,map } from "rxjs/operators";
+import { IProduct } from "./product";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private productUrl = 'api/products/products.json';
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.productUrl).pipe(
+      tap(data => console.log('All', JSON.stringify(data))),
+      catchError(this.handleErrror)
+    );
+  }
+
+  // Get one product
+// Since we are working with a json file, we can only retrieve all products
+// So retrieve all products and then find the one we want using 'map'
+  getProduct(id: number): Observable < IProduct | undefined > {
+  return this.getProducts()
+    .pipe(
+      map((products: IProduct[]) => products.find(p => p.productId === id))
+    );
+  }
+
+    handleErrror(err: HttpErrorResponse) {
+      let errorMessage = '';
+      if (err.error instanceof ErrorEvent) {
+        errorMessage = `error occuerred: ${err.error.message}`
+      } else {
+        errorMessage = `server returned code: ${err.status}, error message is ${err.message}`;
+      }
+      console.error(errorMessage);
+      return throwError(errorMessage);
+    }
+}
